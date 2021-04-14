6e2114b5d2375bd59296b9765547d7676b90aec2
HeiSian96 <anais.fenandez.villar1996@gmail.com>
2021-04-14 16:47:28 +0200

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-const name = 'world';
 
-console.log(`Hello ${name}`); 
+/*menu responsive*/
+const hamburguer = document.querySelector('.menu-ham');
+const links = document.querySelector('ul');
+const bars = document.querySelectorAll('.menu-ham span');
+
+hamburguer.addEventListener('click', () => {
+	links.classList.toggle('show');
+	bars.forEach(child => {child.classList.toggle('animation')});
+});
+
+/*validación de formulario*/
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const errorname = document.getElementById('warning-name');
+const erroremail = document.getElementById('warning-email');
+const errormessage = document.getElementById('warning-message');
+
+form.addEventListener('submit', evt =>{
+	evt.preventDefault();
+	let warningname = '';
+	let warningemail = '';
+	let warningmessage = '';
+	let getin = false;
+	let regexpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+	errorname.innerHTML = '';
+	erroremail.innerHTML = '';
+	errormessage.innerHTML = '';
+
+	if(name.value.length <=2 || name.value.length >=40){
+		warningname += `El nombre debe tener entre 2 y 40 caracteres.`;
+		getin = true;
+	}
+
+	if(!regexpEmail.test(email.value)){
+		warningemail += `El formato de email que se ha introducido es incorrecto.`;
+		getin = true;
+	}
+	if(message.value.length <=5 || message.value.length >=250){
+		warningmessage += `El mensaje debe tener entre 5 y 250 caracteres.`
+		getin = true;
+	}
+
+	if(getin){
+		errorname.innerHTML = warningname;
+		erroremail.innerHTML = warningemail;
+		errormessage.innerHTML = warningmessage;
+	}
+});
+
+
