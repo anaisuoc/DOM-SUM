7299321321c1986da13b2f078ea2c1eafc2fe842
Anaís Fernández Villar <anais_fernandez_villar@MacBook-Air-de-Anais.local>
2021-05-28 17:35:51 +0200

    add

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 /*slider*/
 let box = document.querySelectorAll('.box-clip-path');
-box.forEach(function (popup) {
-	popup.addEventListener('click', function () {
-		popup.classList.toggle('active');
+box.forEach( function(popup){ popup.addEventListener('click', 
+function(){
+	popup.classList.toggle('active');
 	});
 });
