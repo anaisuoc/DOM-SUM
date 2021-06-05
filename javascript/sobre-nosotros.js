47b05ab661ff17ebe92a3304542a89d8e93ca663
Anaís Fernández Villar <anais_fernandez_villar@MacBook-Air-de-Anais.local>
2021-06-05 18:43:23 +0200

    fontawesome

--- javascript/sobre-nosotros.js
+++ javascript/sobre-nosotros.js
@@ -11,6 +11,19 @@ hamburguer.addEventListener('click', function () {
 	});
 });
 
+/*lazy loading*/
+if ('loading' in HTMLImageElement.prototype) {
+	const images = document.querySelectorAll("img.lazyload");
+	images.forEach(img => {
+		img.src = img.dataset.src;
+	});
+} else {
+	let script = document.createElement("script");
+	script.async = true;
+	script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
+	document.body.appendChild(script);
+}
+
 /*hora en Bangkok*/
 let date = moment();
 date.tz('Asia/Bangkok');
