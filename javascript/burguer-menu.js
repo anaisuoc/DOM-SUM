879a0f4bdd42a09727ff8b378692816c8e78facd
Anaís Fernández Villar <anais_fernandez_villar@MacBook-Air-de-Anais.local>
2021-06-09 17:56:40 +0200

    Commit

--- javascript/burguer-menu.js
+++ javascript/burguer-menu.js
@@ -9,16 +9,3 @@ hamburguer.addEventListener('click', function () {
 		child.classList.toggle('animation');
 	});
 });
-
-/*lazy loading*/
-if ('loading' in HTMLImageElement.prototype) {
-	const images = document.querySelectorAll("img.lazyload");
-	images.forEach(img => {
-		img.src = img.dataset.src;
-	});
-} else {
-	let script = document.createElement("script");
-	script.async = true;
-	script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
-	document.body.appendChild(script);
-}
--- javascript/index.js
+++ javascript/index.js
@@ -10,19 +10,6 @@ hamburguer.addEventListener('click', function () {
 	});
 });
 
-/*lazy loading*/
-if ('loading' in HTMLImageElement.prototype) {
-	const images = document.querySelectorAll("img.lazyload");
-	images.forEach(img => {
-		img.src = img.dataset.src;
-	});
-} else {
-	let script = document.createElement("script");
-	script.async = true;
-	script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
-	document.body.appendChild(script);
-}
-
 /*slider*/
 let box = document.querySelectorAll('.box-clip-path');
 box.forEach( function(popup){ popup.addEventListener('click', 
