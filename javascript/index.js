e67c2ecb0f35f815eb025c1c7646096547ae2e0c
Anaís Fernández Villar <anais_fernandez_villar@MacBook-Air-de-Anais.local>
2021-06-10 10:49:20 +0200

    Height

--- javascript/index.js
+++ javascript/index.js
@@ -12,8 +12,9 @@ hamburguer.addEventListener('click', function () {
 
 /*slider*/
 let box = document.querySelectorAll('.box-clip-path');
-box.forEach( function(popup){ popup.addEventListener('click', 
-function(){
-	popup.classList.toggle('active');
-	});
+box.forEach(function (popup) {
+	popup.addEventListener('click',
+		function () {
+			popup.classList.toggle('active');
+		});
 });
